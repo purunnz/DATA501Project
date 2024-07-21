6d0e28bd347621348076a2e2066b22f669f352a3
purunnz <purun.nz@gmail.com>
2024-07-21 17:09:14 +1200

    1. add optional input ( qqplot ) in function. 2. 3 types of input data ( manual, unform, normal )

--- Ass1.R
+++ Ass1.R
@@ -1,24 +1,43 @@
+# Load library 
+library(ggplot2)
+
 # Define the custom function
-shapiro_wilk_test <- function(data) {
+shapiro_wilk_test <- function(data, dis_qqplot = FALSE) {
+  
   # Perform the Shapiro-Wilk test
   test_result <- shapiro.test(data)
   
   # Print the result
   print(test_result)
   
+  # qq plot 
+  if (dis_qqplot) {
+    # QQplot of normally distributed values
+    qqnorm(data)
+    # Add qqline to plot
+    qqline(data, col = "darkgreen")
+    
+  }
+  
   # Return the test result
   return(test_result)
 }
 
-# 1: Load your data
+# 1: Load data
 sample_data <- c(4.2, 5.3, 6.1, 7.4, 8.0, 4.8, 5.9, 6.2, 7.5, 5.7)
 
+set.seed(500)
+# Create random normally distributed values
+sample_datan <- rnorm(100)
 
-# 2: Perform the Shapiro-Wilk test
-shapiro_test_result <- shapiro_wilk_test(sample_data)
+# Create random uniformlly distributed values
+sample_datau <- runif(100)
 
 
-# 3: Print the result
-print(shapiro_test_result)
+# 2: Perform the Shapiro-Wilk test & qqplot 
+shapiro_test_result <- shapiro_wilk_test(sample_data, dis_qqplot = TRUE)
+shapiro_test_result <- shapiro_wilk_test(sample_datan, dis_qqplot = TRUE)
+shapiro_test_result <- shapiro_wilk_test(sample_datau, dis_qqplot = TRUE)
+
 
 
